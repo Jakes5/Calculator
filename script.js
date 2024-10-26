9543111620186c1ee9c12940ccc34bbadffee061
JHvdSpuy <86352532+Jakes5@users.noreply.github.com>
2024-10-26 20:27:55 +0200

    numpad entry display and click display (on all button) display

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
+
+// behaviour of pressed key
 const delay = ms => new Promise(res => setTimeout(res, ms));
 const keyPress = async (keyCode) => {
     await delay(100);
     document.querySelector(`#${keyCode}`).classList.remove('active');
-    console.log("Waited 2s");
-  
-    // await delay(5000);
-    // console.log("Waited an additional 5s");
   };
+const calcDisplay = document.querySelector('.calc-display');
+const buttons = document.querySelectorAll('.buttons');
 
 
 function add(a, b){
@@ -29,16 +29,18 @@ function divide(a, b){
 function operate(num1, num2, operator){
     return operator(num1, num2)
 };
-6
+
 // variables to update your display later
 let firstNumber;
 let secondNumber;
 let operator;
+let display = '';
 
 
 console.log(operate(1,2,add));
 
 document.addEventListener('keydown', (e)=>{
+    
     // let numpadRegex = new RegExp(/(Numpad)/);
     // const num = document.querySelector('#num7');
     let keyCode;
@@ -46,11 +48,49 @@ document.addEventListener('keydown', (e)=>{
         keyCode = e.code;
         console.log(keyCode);
         document.querySelector(`#${keyCode}`).classList.add('active');
-        // num7.classList.add('active');
         keyPress(keyCode);
+        //check length of calc space
+        if(display.length < 11){
+            display += e.key;
+            calcDisplay.textContent = display;
+        }
+
+
+    } else if(e.key === 'Escape'){
+        display = ''
+        calcDisplay.textContent = '0';
     }
     console.log(e);
     // console.log(num7);
     
+})
+
+let calcButtons = Array.from(buttons);
+// for(btn of calcButtons){
+//     console.log(btn);
+    
+// }
+console.log(calcButtons);
+
+buttons.forEach((btn, index, array) =>{
+    btn.addEventListener('click', () =>{
+    //check length of calc space
+        if(display.length < 11){
+            let input = btn.textContent
+            display += input
+            calcDisplay.textContent = display;
+        }
+
+    })
     
 })
+
+// buttons.addEventListener('click', () =>{
+//     let calcButtons = Array.from(buttons);
+//     // for(btn of calcButtons){
+//     //     console.log(btn);
+        
+//     // }
+//    console.log(calcButtons);
+     
+// })
