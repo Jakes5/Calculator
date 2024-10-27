b303ae2c64b7d9a40e1e2c84be1de2e4c47c43fd
JHvdSpuy <86352532+Jakes5@users.noreply.github.com>
2024-10-27 22:42:39 +0200

    numbers and delete buttons functional on click and keydown

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const keyPress = async (keyCode) => {
   };
 const calcDisplay = document.querySelector('.calc-display');
 const buttons = document.querySelectorAll('.buttons');
+const numbers = document.querySelectorAll('.numbers');
+const deletions = document.querySelectorAll('.deletions');
+
+const operatorBtn = ['Minus','Equal', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'NumpadAdd', 'NumpadEnter']
+const numericalBtn = ['Numpad0','Numpad1','Numpad2','Numpad3','Numpad4','Numpad5','Numpad6','Numpad7','Numpad8','Numpad9','Digit0','Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9'];
+const decimalsBtn = ['NumpadDecimal', 'Period'];
 
 
 function add(a, b){
@@ -30,6 +36,10 @@ function operate(num1, num2, operator){
     return operator(num1, num2)
 };
 
+function checkDecimal(){
+    
+}
+
 // variables to update your display later
 let firstNumber;
 let secondNumber;
@@ -37,32 +47,83 @@ let operator;
 let display = '';
 
 
-console.log(operate(1,2,add));
+// console.log(operate(1,2,add));
 
-document.addEventListener('keydown', (e)=>{
-    
-    // let numpadRegex = new RegExp(/(Numpad)/);
-    // const num = document.querySelector('#num7');
-    let keyCode;
-    if(e.code.startsWith("Numpad") || e.code.startsWith("Back")){
-        keyCode = e.code;
-        console.log(keyCode);
-        document.querySelector(`#${keyCode}`).classList.add('active');
-        keyPress(keyCode);
-        //check length of calc space
+deletions.forEach((delBtn, index, array) =>{
+    delBtn.addEventListener('click', () =>{
+        let input = delBtn.textContent;
+        if(input === 'CLR'){
+            display = ''
+            calcDisplay.textContent = '0';
+        } else if (input === 'DEL'){
+            if(display[display.length] -2 === '.'){
+                display = display.slice(0, display.length - 2);
+                calcDisplay.textContent = display;
+                //reset to ZERO
+            } else if (display.length === 1 || display.length === 0){
+                display = ''
+                calcDisplay.textContent = '0';
+            }else{
+                console.log(display.length);
+                display = display.slice(0, -1);
+                calcDisplay.textContent = display;
+            }
+        }
+    })
+})
+
+
+numbers.forEach((btn, index, array) =>{
+    btn.addEventListener('click', () =>{
+        let input = btn.textContent;
+    //check length of calc space
         if(display.length < 11){
-            display += e.key;
+            display += input
             calcDisplay.textContent = display;
+            console.log(display); 
         }
 
+    })
+    
+})
 
-    } else if(e.key === 'Escape'){
+document.addEventListener('keydown', (e)=>{
+    let keyCode = e.code.startsWith("Digit") ? e.code.replace("Digit", "Numpad") : e.code;
+    //Numerical buttons
+    if(numericalBtn.includes(keyCode) && display.length < 11){
+        document.querySelector(`#${keyCode}`).classList.add('active');
+        keyPress(keyCode);
+        display += e.key;
+        calcDisplay.textContent = display;
+        //check length of calc space
+    }   else if(e.key === 'Escape'){
         display = ''
         calcDisplay.textContent = '0';
+    }   else if(e.key === 'Backspace'){
+        if(display[display.length] -2 === '.'){
+            display = display.slice(0, display.length - 2);
+            calcDisplay.textContent = display;
+            //reset to ZERO
+        } else if (display.length === 1 || display.length === 0){
+            display = ''
+            calcDisplay.textContent = '0';
+        }else{
+            console.log(display.length);
+            display = display.slice(0, -1);
+            calcDisplay.textContent = display;
+        }
     }
-    console.log(e);
-    // console.log(num7);
-    
+    if(decimalsBtn.includes(keyCode) && !display.includes('.')){
+        //make this a function with '!display.includes('.')'
+        display += e.key
+        calcDisplay.textContent = display;
+        console.log(display);   
+    }
+
+    if(operatorBtn.includes(keyCode)){
+        console.log(display);       
+    }
+
 })
 
 let calcButtons = Array.from(buttons);
@@ -72,18 +133,6 @@ let calcButtons = Array.from(buttons);
 // }
 console.log(calcButtons);
 
-buttons.forEach((btn, index, array) =>{
-    btn.addEventListener('click', () =>{
-    //check length of calc space
-        if(display.length < 11){
-            let input = btn.textContent
-            display += input
-            calcDisplay.textContent = display;
-        }
-
-    })
-    
-})
 
 // buttons.addEventListener('click', () =>{
 //     let calcButtons = Array.from(buttons);
